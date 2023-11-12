fc401d815ff86c54a10afc78ec83407d4cb2906f
Alex <godrak118@gmail.com>
2023-11-12 16:24:10 +1100

    signal fully working need to add function to record fliping result

--- server/main.cpp
+++ server/main.cpp
@@ -1,18 +1,16 @@
 #include "server.h"
-#include <locale>
 #include <csignal>
 #include <iostream>
 
 FlipCoinServer server;
 void signal_handler(int signal_num) {
     std::cout << "Signal " << signal_num << " received, terminating the server." << std::endl;
-    server.stop(); // 调用服务器的停止方法
+    server.stop();
     exit(signal_num);
 }
 
 int main() {
-    // 设置C++全局locale为C locale
-    std::locale::global(std::locale("C"));
+
     signal(SIGTERM, signal_handler);
     signal(SIGINT, signal_handler);
     server.run();
--- server/server.cpp
+++ server/server.cpp
@@ -7,18 +7,15 @@
 #include <random>
 
 FlipCoinServer::FlipCoinServer() : server_fd(0), addrlen(sizeof(address)), is_running(false) {
-    // 创建socket文件描述符
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // 定义socket类型
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
-    // 绑定socket到端口
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -36,16 +33,23 @@ FlipCoinServer::~FlipCoinServer() {
 }
 
 void FlipCoinServer::run() {
+    is_running = true;
     int new_socket;
     long valread;
-    is_running = true;
+
 
 
     while (is_running) {
         std::cout << "Server started. Waiting for connections..." << std::endl;
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
+
+            if(is_running) {
+                perror("accept");
+                continue;
+            } else {
+                std::cout << "Server is shutting down." << std::endl;
+                break;
+            }
         }
 
         char buffer[1024] = {0};
@@ -55,6 +59,8 @@ void FlipCoinServer::run() {
         std::cout << "Coin flipped: " << coin_result << std::endl;
         close(new_socket);
     }
+
+    close(server_fd);
 }
 
 std::string FlipCoinServer::flip_coin() {
